2d2e412b6a3c6d71d04c79bca51543b53206df6c
ShahidC0de <shahidzada.cs@gmail.com>
2025-02-24 23:08:36 +0500

    working on user edit profile screen

--- lib/features/user_profile/presentation/screens/edit_profile_screen.dart
+++ lib/features/user_profile/presentation/screens/edit_profile_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:twitter_clone/core/common/loader.dart';
 import 'package:twitter_clone/core/cubits/app_user/app_user_cubit.dart';
 import 'package:twitter_clone/core/entities/user_entity.dart';
 import 'package:twitter_clone/core/theme/pallete.dart';
+import 'package:twitter_clone/core/utils/utilities.dart';
 
 class EditProfileScreen extends StatefulWidget {
   static route() =>
@@ -15,7 +18,20 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
+  List<File> pickedImages = [];
+  final nameController = TextEditingController();
+  final bioController = TextEditingController();
   UserEntity? currentUser;
+  void onPickImage() async {
+    pickedImages = await pickImages();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    bioController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +39,20 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     final state = blocProvider.state;
     currentUser = (state is AppUserLoggedIn) ? currentUser = state.user : null;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+        centerTitle: false,
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Save',
+                style: TextStyle(
+                  color: Pallete.blueColor,
+                ),
+              ))
+        ],
+      ),
       body: currentUser == null
           ? const Loader()
           : Column(
@@ -30,14 +60,20 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                 SizedBox(
                   height: 200,
                   child: Stack(children: [
-                    Positioned.fill(
+                    Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        width: double.infinity,
+                        height: 150,
                         child: currentUser!.bannerPic.isEmpty
                             ? Container(
                                 color: Pallete.blueColor,
                               )
                             : Image.network(currentUser!.bannerPic)),
                     Positioned(
-                      bottom: 0,
+                      bottom: 20,
+                      left: 20,
                       child: CircleAvatar(
                         backgroundImage: NetworkImage(
                           currentUser!.profilePic,
@@ -46,6 +82,24 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                       ),
                     ),
                   ]),
+                ),
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    hintText: currentUser!.name,
+                    contentPadding: const EdgeInsets.all(18),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  controller: bioController,
+                  decoration: InputDecoration(
+                    hintText: currentUser!.bio,
+                    contentPadding: const EdgeInsets.all(18),
+                  ),
+                  maxLength: 4,
                 )
               ],
             ),
